e63373302c4a4dbbf744d15d9363963ef0d0c63a
Brian Upton <ystros@gmail.com>
2020-06-25 18:36:42 -0700

    Add support for pods on the restricted list

--- src/RestrictedList.js
+++ src/RestrictedList.js
@@ -1,8 +1,7 @@
-const moment = require('moment');
-
 class RestrictedList {
     constructor(rules) {
         this.rules = rules;
+        this.pods = rules.pods || [];
     }
 
     validate(cards) {
@@ -19,6 +18,10 @@ class RestrictedList {
             errors.push(`${this.rules.name}: Contains cards that are not tournament legal: ${bannedCardsOnList.map(card => card.name).join(', ')}`);
         }
 
+        for(const pod of this.pods) {
+            errors = errors.concat(this.validateRestrictedPods({ pod, cards }));
+        }
+
         return {
             name: this.rules.name,
             valid: errors.length === 0,
@@ -28,6 +31,23 @@ class RestrictedList {
             restrictedCards: restrictedCardsOnList,
             bannedCards: bannedCardsOnList
         };
+    }
+
+    validateRestrictedPods({ pod, cards }) {
+        const errors = [];
+
+        const restrictedCard = cards.find(card => card.code === pod.restricted);
+
+        if(!restrictedCard) {
+            return errors;
+        }
+
+        const cardsOnList = cards.filter(card => pod.cards.includes(card.code));
+        if(cardsOnList.length > 0) {
+            errors.push(`${this.rules.name}: ${cardsOnList.map(card => card.name).join(', ')} cannot be used with ${restrictedCard.name}`);
+        }
+
+        return errors;
     }
 }
 
