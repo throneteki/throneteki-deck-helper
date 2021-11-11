9aeba9f262741c4ee293fbfa313661c792fb5d6b
Brian Upton <ystros@gmail.com>
2021-11-10 21:28:07 -0800

    Fix drafted cards check
    By not returning true when the deck is valid, the error message was
    always being returned.

--- src/Formats/DraftFormat.js
+++ src/Formats/DraftFormat.js
@@ -14,13 +14,19 @@ const DraftFormat = {
 
                 const allCards = deck.getAllCards();
 
+                let onlyIncludesDraftedCards = true;
+
                 for(const cardQuantity of allCards) {
                     if(!draftCardQuantityByCode.has(cardQuantity.card.code)) {
+                        onlyIncludesDraftedCards = false;
                         errors.push(`${cardQuantity.card.name} is not one of the drafted cards`);
                     } else if(draftCardQuantityByCode.get(cardQuantity.card.code) < cardQuantity.count) {
+                        onlyIncludesDraftedCards = false;
                         errors.push(`${cardQuantity.card.name} has ${cardQuantity.count} copies but only ${draftCardQuantityByCode.get(cardQuantity.card.code)} copies were drafted`);
                     }
                 }
+
+                return onlyIncludesDraftedCards;
             }
         }
     ]
